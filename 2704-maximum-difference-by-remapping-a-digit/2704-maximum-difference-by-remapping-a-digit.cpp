3cc1a8d573dd5aba93cee8c2c15fb5efa77e1410
siddhantsachan <149047311+siddhantsachan@users.noreply.github.com>
2025-06-15 12:53:35 +0530

    Time: 4 ms (1.71%), Space: 10.2 MB (5.14%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int minMaxDifference(int num) {
-        string s = to_string(num);
-        string t = s;
-        size_t pos = s.find_first_not_of('9');
-        if (pos != string::npos) {
-            char a = s[pos];
-            replace(s.begin(), s.end(), a, '9');
+        int mx,mn;
+        mx=mn=num;
+        for(int d1=0;d1<=9;d1++){
+            for(int d2=0;d2<=9;d2++){
+                if(d1==d2) continue;
+                string str=to_string(num);
+                for(int i=0;i<str.size();i++){
+                    if(str[i]-'0'==d1){
+                        str[i]=d2+'0';
+                    }
+                }
+                mx=max(mx,stoi(str));
+                mn=min(mn,stoi(str));
+            }
         }
-        char b = t[0];
-        replace(t.begin(), t.end(), b, '0');
-        return stoi(s) - stoi(t);
+        return mx-mn;
     }
 };
